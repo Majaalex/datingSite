ef4fcee742ce89ae2f0ce5b9c0ed4952fa302063
AMaja <alex-maja@hotmail.com>
2018-04-11 12:21:19 +0300

    base..

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
     "http://www.w3.org/TR/html4/strict.dtd">
 
@@ -14,24 +13,4 @@
         <div><a href="#">Home</a></div>
         <div><a href="#">Profile</a></div>
         <div><a href="#">Browse</a></div>
-=======
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-    "http://www.w3.org/TR/html4/strict.dtd">
-
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Simple Site</title>
-</head>
-
-<body>
-<div id="header">
-    <h1>Simple Site</h1>
-    <ul class="nav global">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Articles</a></li>
-        <li><a href="#">Portfolio</a></li>
-    </ul>
-
->>>>>>> 40a3c54992eef2dfa03acd71374af6867bc88a83
 </div>
--- resources/templates/rightPanel.php
+++ resources/templates/rightPanel.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div id="siteControls">
     <ul class="categories">
         <li>PHP</li>
@@ -9,16 +8,4 @@
         <!-- ads code -->
     </div>
 
-=======
-<div id="siteControls">
-    <ul class="categories">
-        <li>PHP</li>
-        <li>HTML</li>
-        <li>CSS</li>
-    </ul>
-    <div class="ads">
-        <!-- ads code -->
-    </div>
-
->>>>>>> 40a3c54992eef2dfa03acd71374af6867bc88a83
 </div>
