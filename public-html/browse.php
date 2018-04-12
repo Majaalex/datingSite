b9482d9ad533bb38f9c9434c90497ffb53f8e2db
Skelis <alex-maja@hotmail.com>
2018-04-12 19:21:20 +0300

    Add files via upload

--- public-html/browse.php
+++ public-html/browse.php
@@ -6,7 +6,7 @@ require_once(TEMPLATES_PATH . "/header.php");
 ?>
     <div id="container">
         <div id="content">
-            <!-- content -->
+            <p>Hello my friend</p>
         </div>
         <?php
         require_once(TEMPLATES_PATH . "/rightPanel.php");
