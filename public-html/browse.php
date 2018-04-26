33fb1e2bc663974e6d89bf959eae06e865fc8da1
Alexander Maja <alex-maja@hotmail.com>
2018-04-26 18:18:44 +0300

    Add files via upload

--- public-html/browse.php
+++ public-html/browse.php
@@ -12,77 +12,125 @@ require_once(TEMPLATES_PATH . "/header.php");
         <div id="filter-container">
             <div id="filter-content">
                 <form id="browse-set">
-            <table>
-                <tr>
-                    <td>
-                        Searching for:<br>
-                        <label for="male">Male:</label> <input type="checkbox" value="true" name="male"><br>
-                        <label for="female">Female:</label> <input type="checkbox" value="true" name="female"><br>
-                        <label for="other">Other:</label> <input type="checkbox" value="true" name="other"><br>
-                    </td>
-                    <td>
+                    <table>
+                        <tr>
+                            <td>
+                                Searching for:<br>
+                                <label for="male">Male:</label> <input type="checkbox" value="true" name="male"><br>
+                                <label for="female">Female:</label> <input type="checkbox" value="true"
+                                                                           name="female"><br>
+                                <label for="other">Other:</label> <input type="checkbox" value="true" name="other"><br>
+                            </td>
+                            <td>
 
-                        <label>Minimum salary:
-                            <select name="minSalary">
-                                <option value="0">0</option>
-                                <option value="500">500</option>
-                                <option value="1000">1 000</option>
-                                <option value="2500">2 500</option>
-                                <option value="5000">5 000</option>
-                                <option value="10000">10 000</option>
-                                <option value="25000">25 000</option>
-                                <option value="50000">50 000</option>
-                                <option value="100000">100 000</option>
-                                <option value="250000">250 000</option>
-                                <option value="500000">500 000</option>
-                                <option value="1000000">1 000 000</option>
-                        </select></label>
-                        <label>
-                            <?php
-                            if (isset($_SESSION['id'])){
-                                $curr = db::instance()->get("SELECT currency FROM users WHERE username = ?", array($_SESSION['id']));
-                                echo $curr[0]["currency"];
-                            } else {
-                                echo "USD";
-                            }
-                            ?>
-                        </label>
-                    </td>
-                    <td>
-                        <label>Maximum salary:
-                            <select name="maxSalary">
-                                <option value="500">500</option>
-                                <option value="1000">1 000</option>
-                                <option value="2500">2 500</option>
-                                <option value="5000">5 000</option>
-                                <option value="10000">10 000</option>
-                                <option value="25000">25 000</option>
-                                <option value="50000">50 000</option>
-                                <option value="100000">100 000</option>
-                                <option value="250000">250 000</option>
-                                <option value="500000">500 000</option>
-                                <option value="1000000">1 000 000</option>
-                                <option value="2000000">2 000 000+</option>
-                        </select></label>
-                        <label>
-                            <?php
-                            if (isset($_SESSION['id'])){
-                                echo $curr[0]["currency"];
-                            } else {
-                                echo "USD";
-                            }
-                            ?>
-                        </label>
-                    </td>
-                    <td>
-                        <input type="button" value="Search" id="filter-button">
-                    </td>
-                </tr>
-            </table>
+                                <label>Minimum salary:
+                                    <select name="minSalary">
+                                        <option value="0">0</option>
+                                        <option value="500">500</option>
+                                        <option value="1000">1 000</option>
+                                        <option value="2500">2 500</option>
+                                        <option value="5000">5 000</option>
+                                        <option value="10000">10 000</option>
+                                        <option value="25000">25 000</option>
+                                        <option value="50000">50 000</option>
+                                        <option value="100000">100 000</option>
+                                        <option value="250000">250 000</option>
+                                        <option value="500000">500 000</option>
+                                        <option value="1000000">1 000 000</option>
+                                    </select></label>
+                                <label>
+                                    <?php
+                                    if (isset($_SESSION['id'])) {
+                                        $curr = db::instance()->get("SELECT currency FROM users WHERE username = ?", array($_SESSION['id']));
+                                        echo $curr[0]["currency"];
+                                    } else {
+                                        echo "USD";
+                                    }
+                                    ?>
+                                </label>
+                            </td>
+                            <td>
+                                <label>Maximum salary:
+                                    <select name="maxSalary">
+                                        <option value="500">500</option>
+                                        <option value="1000">1 000</option>
+                                        <option value="2500">2 500</option>
+                                        <option value="5000">5 000</option>
+                                        <option value="10000">10 000</option>
+                                        <option value="25000">25 000</option>
+                                        <option value="50000">50 000</option>
+                                        <option value="100000">100 000</option>
+                                        <option value="250000">250 000</option>
+                                        <option value="500000">500 000</option>
+                                        <option value="1000000">1 000 000</option>
+                                        <option value="2000000">2 000 000+</option>
+                                    </select></label>
+                                <label>
+                                    <?php
+                                    if (isset($_SESSION['id'])) {
+                                        echo $curr[0]["currency"];
+                                    } else {
+                                        echo "USD";
+                                    }
+                                    ?>
+                                </label>
+                            </td>
+                            <td>
+                                <label>Order by:
+                                    <select name="order">
+                                        <option value="1">Salary ascending</option>
+                                        <option value="2">Salary descending</option>
+                                        <option value="3">First name ascending</option>
+                                        <option value="4">First name descending</option>
+                                        <option value="5">Surname ascending</option>
+                                        <option value="6">Surname descending</option>
+                                    </select></label>
+                            </td>
+                            <td>
+                                <label>Select currency (Default is your account setting or USD):
+                                    <select name="currency" id="currencySelect">
+
+                                        // sets the currency variable for JS depending on the user is logged in, or to
+                                        USD if there's nobody logged in
+                                        <script>
+                                            <?php
+                                            if(isset($_SESSION['id'])) {
+                                            $user = DB::instance()->get("SELECT currency FROM users WHERE username = ?", array($_SESSION['id']));
+                                            ?>
+                                            var currency = '<?php echo $user[0]["currency"]; ?>'; <?php
+                                            } else {
+                                                if (!isset($_SESSION['id'])) {
+                                                    echo "var currency = 'USD'";
+                                                };
+                                            }
+                                            ?>
+                                        </script>
+                                        <?php
+
+
+                                        if (isset($_SESSION['id'])) {
+                                            echo "<option selected='selected'>" . $user[0]["currency"] . "</option>";
+                                        }
+                                        require_once TEMPLATES_PATH . '/currency.html';
+                                        ?>
+                                        <script>
+                                            $(document).ready(function () {
+                                                currency = $("currencySelect").find(":selected").text();
+                                            });
+
+                                        </script>
+                                    </select></label>
+                            </td>
+                            <td>
+                                <input type="button" value="Search" id="filter-button">
+                            </td>
+                        </tr>
+                    </table>
                 </form>
             </div>
         </div>
         <?php
+
         $id = 0;
         $array = db::instance()->get("SELECT * FROM users", array());
         $count = db::instance()->count("SELECT * FROM users", array("0"));
@@ -92,20 +140,20 @@ require_once(TEMPLATES_PATH . "/header.php");
 
         // Loops out all requested values in a neat table format
         $i = 0;
-        for ($row = 0; $row < $count / 5; $row++){
+        for ($row = 0; $row < $count / 5; $row++) {
             echo "<tr>";
-            for ($col = 0; $col < 5; $col++){
-                if ($i < $count){
+            for ($col = 0; $col < 5; $col++) {
+                if ($i < $count || $i < 10) {
                     echo "<td class='browse'>";
                     echo "<div>";
                     echo "<ul>";
-                    echo "<li><a href='" . url_for("profile.php"). "?user=".  h($array[$i]["username"])  . "'>" . h($array[$i]["username"]) . "</a></li>";
+                    echo "<li><a href='" . url_for("profile.php") . "?user=" . h($array[$i]["username"]) . "'>" . h($array[$i]["username"]) . "</a></li>";
                     echo "<li>" . h($array[$i]["firstname"]) . " " . h($array[$i]["lastname"]) . "</li>";
                     echo "<li>" . h($array[$i]["about"]) . "</li>";
-                    if (isset($_SESSION['id'])){
+                    if (isset($_SESSION['id'])) {
                         echo "<li>" . h($array[$i]["email"]) . "</li>";
                     }
-                    echo "<li>" . h($array[$i++]["salary"]) . "</li>";
+                    echo "<li class='salary'>" . h($array[$i++]["salary"]) . "</li>";
                     echo "</ul>";
                     echo "</div>";
                     echo "</td>";
@@ -121,14 +169,32 @@ require_once(TEMPLATES_PATH . "/header.php");
         ?>
     </div>
 </div>
+
 <script>
     // sends form data as GET to browse_content.php, and returns it dynamically based on the filter options
-    $(document).ready(function(){
-        $("#filter-button").click(function(){
+    $(document).ready(function () {
+        $("#filter-button").click(function () {
             var get_data = $('#browse-set').serialize();
             $("#browse").load("browse_content.php", get_data);
-
         });
+    });
+
+
+    // gets data from localstorage which is then used to convert the loaded data on the page to corresponding currency
+    $(document).ready(function () {
+        /*$.get('https://openexchangerates.org/api/latest.json', {app_id: '9d60f5c4e53c43898ee378509406c5c9'}, function (data) {
+            var jsonData = JSON.stringify(data.rates);
+            localStorage.setItem("openCurrency", jsonData);
+        });*/
+        var jsonRetrieveData = localStorage.getItem("openCurrency");
+        jsonRetrieveData = JSON.parse(jsonRetrieveData);
+        jsonRetrieveData = jQuery.makeArray(jsonRetrieveData);
+        //goes through each li containing salary and updates the value
+        $(".salary").each(function () {
+            var text = $(this).text();
+            var converted = text * jsonRetrieveData[0][currency];
+            $(this).text(converted.toFixed(2) + " " + currency)
+        })
     });
 </script>
 <?php
--- public-html/browse_content.php
+++ public-html/browse_content.php
@@ -6,7 +6,7 @@ require_once("../resources/config.php");
 require_once "../resources/functions.php";
 $i = 0;
 
-// Makes sure there are always values set
+// Makes sure there are always values set and sorts based on previous options
 if (!isset($_GET['female'])){
     $_GET['female'] = null;
 }
@@ -16,11 +16,34 @@ if (!isset($_GET['male'])){
 if (!isset($_GET['other'])){
     $_GET['other'] = null;
 }
+if (isset($_GET['order'])){
+    switch($_GET['order']){
+        case 1:
+            $order = 'salary ASC';
+            break;
+        case 2:
+            $order = 'salary DESC';
+            break;
+        case 3:
+            $order = 'firstname ASC';
+            break;
+        case 4:
+            $order = 'firstname DESC';
+            break;
+        case 5:
+            $order = 'lastname ASC';
+            break;
+        case 6:
+            $order = 'lastname DESC';
+            break;
+    }
+}
+var_dump($_POST);
 
 // Queries based on what the user has set in browse.php query
 $count = db::instance()->count("SELECT * FROM users WHERE salary > ? AND salary < ? OR gender = ? OR gender = ? OR gender = ?",
     array($_GET['minSalary'], $_GET['maxSalary'] , $_GET['female'], $_GET['male'], $_GET['other']));
-$array = db::instance()->get("SELECT * FROM users WHERE salary > ? AND salary < ? OR gender = ? OR gender = ? OR gender = ?",
+$array = db::instance()->get("SELECT * FROM users WHERE salary > ? AND salary < ? OR gender = ? OR gender = ? OR gender = ? ORDER BY $order",
     array($_GET['minSalary'], $_GET['maxSalary'] , $_GET['female'], $_GET['male'], $_GET['other']));
 
 // Sames as browse.php, loops out all users in a neat table
@@ -37,7 +60,7 @@ for ($row = 0; $row < $count / 5; $row++){
             if (isset($_SESSION['id'])){
                 echo "<li>" . h($array[$i]["email"]) . "</li>";
             }
-            echo "<li>" . h($array[$i++]["salary"]) . "</li>";
+            echo "<li class='salary'>" . h($array[$i++]["salary"]) . "</li>";
             echo "</ul>";
             echo "</div>";
             echo "</td>";
@@ -48,3 +71,35 @@ for ($row = 0; $row < $count / 5; $row++){
     echo "</tr>";
 }
 echo "</table>";
+?>
+<script>
+    // sets the currency variable for JS depending on the user is logged in, or to USD if there's nobody logged in
+    <?php
+    if(isset($_SESSION['id'])) {
+    $user = DB::instance()->get("SELECT currency FROM users WHERE username = ?", array($_SESSION['id']));
+    ?>
+    var currency = '<?php echo $user[0]["currency"]; ?>'; <?php
+    } else {
+        if (!isset($_SESSION['id'])){
+            echo "var currency = 'USD'";
+        };
+    }
+    ?>
+
+    // gets data from localstorage which is then used to convert the loaded data on the page to corresponding currency
+    $(document).ready(function () {
+        /*$.get('https://openexchangerates.org/api/latest.json', {app_id: '9d60f5c4e53c43898ee378509406c5c9'}, function (data) {
+            var jsonData = JSON.stringify(data.rates);
+            localStorage.setItem("openCurrency", jsonData);
+        });*/
+        var jsonRetrieveData = localStorage.getItem("openCurrency");
+        jsonRetrieveData = JSON.parse(jsonRetrieveData);
+        jsonRetrieveData = jQuery.makeArray(jsonRetrieveData);
+        //goes through each li containing salary and updates the value
+        $(".salary").each(function () {
+            var text = $(this).text();
+            var converted = text * jsonRetrieveData[0][currency];
+            $(this).text(converted.toFixed(2) + " " + currency)
+        })
+    });
+</script>
